2fc26ac1c9bca64120c92714c54c75bb64ca6663
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-09-30 19:30:43 +0300

    Bendrinai tipai

--- BendriniaiDuomenys/Form1.Designer.cs
+++ BendriniaiDuomenys/Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.TextData = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(79, 48);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1266, 762);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 33);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1258, 725);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 33);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 63);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // TextData
+            // 
+            this.TextData.Location = new System.Drawing.Point(1455, 142);
+            this.TextData.Name = "TextData";
+            this.TextData.Size = new System.Drawing.Size(100, 29);
+            this.TextData.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1877, 936);
+            this.Controls.Add(this.TextData);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox TextData;
     }
 }
 
--- BendriniaiDuomenys/Form1.cs
+++ BendriniaiDuomenys/Form1.cs
@@ -12,9 +12,27 @@ namespace BendriniaiDuomenys
 {
     public partial class Form1 : Form
     {
+        private Dictionary<TabPage, string> TextBoxContainer;
+
         public Form1()
         {
             InitializeComponent();
+            TextBoxContainer = new Dictionary<TabPage, string>();
+            TextBoxContainer.Add(tabPage1, "Vienas");
+            TextBoxContainer.Add(tabPage2, "Du");
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            TextData.Text = TextBoxContainer[tabControl1.SelectedTab];
+            Label label = new Label();
+            label.Location = new Point(20, 20);
+            label.Text = "Labas Pasaulis";
+            tabPage1.Controls.Add(label);
+            TextBox textbox = new TextBox();
+            textbox.Location = new Point(40, 40);
+            textbox.Text = "Sveiki visi";
+            tabPage1.Controls.Add(textbox);
         }
     }
 }
